c7c7ed13ad6b5d10dfe736ff45e660553d5957e7
JohanNayvert <presnetsov.evgeniy@gmail.com>
2023-02-11 23:19:13 +0500

    HomeWork Complete

--- src/main/java/com/skypro/recipe/controllers/IngredientsController.java
+++ src/main/java/com/skypro/recipe/controllers/IngredientsController.java
@@ -2,6 +2,12 @@ package com.skypro.recipe.controllers;
 
 import com.skypro.recipe.model.Ingredients;
 import com.skypro.recipe.services.impl.IngredientsServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
@@ -15,26 +21,35 @@ import java.util.Map;
 @RestController
 @RequestMapping("/ingredients")
 @RequiredArgsConstructor
+@Tag(name = "Ингредиенты", description = "CRUD-запросы класса ингредиенты")
 public class IngredientsController {
 
     private final IngredientsServiceImpl ingredientsService;
 
-
-    @PatchMapping
+    @Operation(summary = "Добавление ингредиента")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Ингредиент добавлен")})
+    @Parameters(value = {@Parameter(name = "id", example = "1")})
+    @PostMapping
     Ingredients addIngredients (@Valid @RequestBody Ingredients ingredients){
         return ingredientsService.addIngredients(ingredients);
     }
 
+    @Operation(summary = "Получение ингредиента")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Ингредиент найден")})
     @GetMapping("/{ingredientsId}")
     Ingredients getIngredients (@PathVariable Long ingredientsId){
         return ingredientsService.getIngredients(ingredientsId);
     }
 
+    @Operation(summary = "Изменение ингредиента")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Ингредиент изменен")})
     @PutMapping("/{ingredientsId}")
     Ingredients updateIngredients (@PathVariable Long ingredientsId, @Valid @RequestBody Ingredients ingredients ){
         return ingredientsService.updateIngredients(ingredientsId, ingredients);
     }
 
+    @Operation(summary = "Удаление ингредиента")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Ингредиент удален")})
     @DeleteMapping("/{ingredientsId}")
     Ingredients deleteIngredients (@PathVariable Long ingredientsId){
         return ingredientsService.deleteIngredients(ingredientsId);
--- src/main/java/com/skypro/recipe/controllers/RecipeController.java
+++ src/main/java/com/skypro/recipe/controllers/RecipeController.java
@@ -2,6 +2,10 @@ package com.skypro.recipe.controllers;
 
 import com.skypro.recipe.model.Recipe;
 import com.skypro.recipe.services.impl.RecipeServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
@@ -15,25 +19,34 @@ import java.util.Map;
 @RestController
 @RequestMapping("/recipe")
 @RequiredArgsConstructor
+@Tag(name = "Рецепты", description = "CRUD-запросы класса рецепты")
 public class RecipeController {
 
     private final RecipeServiceImpl recipeService;
 
+    @Operation(summary = "Добавление рецепта")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Рецепт добавлен")})
     @PostMapping
     Recipe addRecipe (@Valid @RequestBody Recipe recipe){
         return recipeService.addRecipe(recipe);
     }
 
+    @Operation(summary = "Получение рецепта")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Рецепт найден")})
     @GetMapping("/{recipeId}")
     Recipe getRecipe (@PathVariable Long recipeId){
         return recipeService.getRecipe(recipeId);
     }
 
+    @Operation(summary = "Изменение рецепта")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Рецепт изменен")})
     @PutMapping("/{recipeId}")
     Recipe updateRecipe (@PathVariable Long recipeId, @Valid @RequestBody Recipe recipe ){
         return recipeService.updateRecipe(recipeId, recipe);
     }
 
+    @Operation(summary = "Удаление рецепта")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Рецепт удален")})
     @DeleteMapping("/{recipeId}")
     Recipe deleteRecipe (@PathVariable Long recipeId){
         return recipeService.deleteRecipe(recipeId);
